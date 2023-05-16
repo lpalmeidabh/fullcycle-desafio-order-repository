9191b393111b21de933fa73e345afea0389f95b7
Lucas Almeida <lpalmeidabh@gmail.com>
2023-05-16 19:52:05 -0300

    customer events successfully created

--- src/domain/event/customer/customer-events.spec.ts
+++ src/domain/event/customer/customer-events.spec.ts
@@ -5,8 +5,7 @@ describe('Customer Events Tests', () => {
 
   it('should send events when user is created', () => {
     const customer = new Customer("123", "Lucas Almeida");
-    
-    
+    expect(customer.id).toBe("123");
   })
 
   it('should send events when user address is updated', () => {
@@ -16,6 +15,8 @@ describe('Customer Events Tests', () => {
 
     const address2 = new Address("Rua 2", 2, "Sao Paulo", "30431121");
     customer.changeAddress(address2)   
+
+    expect(customer.address).toMatchObject(address2);
 
   })
 })
